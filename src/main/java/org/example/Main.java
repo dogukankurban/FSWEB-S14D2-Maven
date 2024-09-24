81330e65381d23aa980df4f66e79413c86d8aeb3
dogukankurban <c.d.kurban007@gmail.com>
2024-09-25 00:04:12 +0300

    s14d2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import org.example.model.*;
+import org.example.model.enums.LampType;
+import org.example.model.enums.PaintColor;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Lamp lamp = new Lamp(LampType.NEON,true,100);
+        Ceiling ceiling = new Ceiling(3, PaintColor.RED);
+        Bed bed = new Bed("double",2,2,2,2);
+        Wall nWall = new Wall("N");
+        Wall eWall = new Wall("E");
+        Wall wWall = new Wall("W");
+        Wall sWall = new Wall("S");
+        Wall[] walls = new Wall[]{nWall,eWall,wWall,sWall};
+
+        Bedroom bedroom = new Bedroom(nWall,eWall,wWall,sWall,ceiling,lamp,
+                new Carpet(2,2,PaintColor.WHITE),
+                "bedroom",bed, new Wardrobe(1,1,100));
+        bedroom.createBedroom();
     }
 }
